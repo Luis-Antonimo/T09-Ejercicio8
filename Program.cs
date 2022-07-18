0aca04d0bdf3cbcdd6efca9cabb307cadda9912e
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-19 00:36:41 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T09_Ejercicio8
+{
+    class Password
+    {
+        private string contraseña;
+        private int longitud;
+
+        public Password()
+        {
+            longitud = 8;
+        }
+
+        public Password(int newlongitud)
+        {
+            Random rdn = new Random();
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%$#@";
+            int listaCaracteres = caracteres.Length;
+            for (int i = 0; i < newlongitud; i++)
+            {
+                char letra = caracteres[rdn.Next(listaCaracteres)];
+                contraseña += letra;
+            }
+            Console.WriteLine(contraseña);
+        }
+    }
+}
